43784f5aa032b36d0e0e735ecb24bf71ab1c8b14
CodingQuell <ryequellhunter@gmail.com>
2020-07-16 15:54:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Take User input
+#Convert input to str
+userInput = str(input("Lend me a variable to make a diamond... "))
+
+#diamond variable 1
+d_1 = userInput+userInput+userInput+userInput+userInput+userInput+userInput+userInput+userInput
+#diamond variable 2
+d_2 = userInput+"  "+userInput
+
+
+print(d_1)
+print(d_2)
+#i tried my best to create a diamond from based off of what you showed me
